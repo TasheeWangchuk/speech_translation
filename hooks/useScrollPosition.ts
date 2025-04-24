52a29a2515ef883b18ace6457aa094f26f27490d
Tashi Wangchuk <tashiwangchuk619@gmail.com>
2025-04-25 00:59:13 +0600

    feat(hook):add useScrollPosition hook

---
+++ hooks/useScrollPosition.ts
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export const useScrollPosition = (): number => {
+  const [scrollPosition, setScrollPosition] = useState<number>(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollPosition(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return scrollPosition;
+};
